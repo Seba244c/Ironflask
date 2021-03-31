e9908c79b7cda60be57accd39e9ca4e086acfd3d
Sebastian Snoer <seb.snoer@gmail.com>
2021-03-31 08:45:10 +0200

    Redid text rendering

--- engine/src/main/java/dk/sebsa/ironflask/engine/gui/objects/Text.java
+++ engine/src/main/java/dk/sebsa/ironflask/engine/gui/objects/Text.java
@@ -2,14 +2,16 @@ package dk.sebsa.ironflask.engine.gui.objects;
 
 import java.util.Map;
 
+import org.lwjgl.opengl.GL11;
+
 import dk.sebsa.ironflask.engine.core.Event;
 import dk.sebsa.ironflask.engine.graph.Mesh2d;
 import dk.sebsa.ironflask.engine.graph.Rect;
 import dk.sebsa.ironflask.engine.graph.Shader;
-import dk.sebsa.ironflask.engine.graph.renderers.Renderer2d;
 import dk.sebsa.ironflask.engine.gui.GuiObject;
 import dk.sebsa.ironflask.engine.gui.text.Glyph;
 import dk.sebsa.ironflask.engine.gui.text.Label;
+import dk.sebsa.ironflask.engine.math.Vector2f;
 
 public class Text extends GuiObject {
 	public Label label;
@@ -24,20 +26,37 @@ public class Text extends GuiObject {
 	}
 	
 	public static void draw(Shader shader, Mesh2d mesh, Rect r, Label label) {
-		Renderer2d.prepare();
 		Map<Character, Glyph> chars = label.font.getChars();
 		float tempX = r.x;
 		char[] c = label.getCharArray();
 		
+		label.font.getTexture().bind();
+		
 		for(int i = 0; i < c.length; i++) {
 			Glyph glyph = chars.get(c[i]);
 			
-			Renderer2d.drawTextureWithTextCoords(label.font.getTexture(),new Rect(tempX, r.y, glyph.scale.x, glyph.scale.y), new Rect(glyph.position.x, glyph.position.y, glyph.size.x, glyph.size.y));
+			// RENDER
+			Rect drawRect = new Rect(tempX, r.y, glyph.scale.x, glyph.scale.y);
+			Rect drawRect2 = new Rect(tempX, r.y, glyph.scale.x, glyph.scale.y);
+			Rect uvRect = new Rect(glyph.position.x, glyph.position.y, glyph.size.x, glyph.size.y);
+			// uvreact
+			float x = uvRect.x + (((drawRect2.x - drawRect.x) / drawRect.width) * uvRect.width);
+			float y = uvRect.y + (((drawRect2.y - drawRect.y) / drawRect.height) * uvRect.height);
+			Rect u = new Rect(x, y, (drawRect2.width / drawRect.width) * uvRect.width, (drawRect2.height / drawRect.height) * uvRect.height);
+			
+			// Draw
+			shader.setUniform("useColor", 0);
+			shader.setUniform("offset", u.x, u.y, u.width, u.height);
+			shader.setUniform("pixelScale", new Vector2f(drawRect2.width, drawRect2.height));
+			shader.setUniform("screenPos", new Vector2f(drawRect2.x, drawRect2.y));
+			
+			Mesh2d.quad.render();
+			// 
 
 			tempX += glyph.scale.x;
 		}
 		mesh.render();
-		Renderer2d.unprepare();
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
 	}
 
 	@Override
